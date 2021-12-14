aeb214511bcb86d2fe0987e783faae5da547516f
Taeyoon Lim <tylim93@naver.com>
2021-12-15 00:38:05 +0900

    Unet Code

---
+++ train.py
@@ -0,0 +1,280 @@
+### import numpy as np
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+# from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms,datasets
+
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+## 라이브러리 추가하기
+import argparse
+
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+# from torch.utils.tensorboard import SummaryWriter
+
+#from model import UNet
+#from dataset import *
+from util import *
+
+import matplotlib.pyplot as plt
+
+from torchvision import transforms, datasets
+# print("@@@UNET@@@:", UNet())
+## Parser 생성하기
+
+
+
+## 네트워크 구축하기
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+
+        def CBR2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True):
+            layers = []
+            layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                                 kernel_size=kernel_size, stride=stride, padding=padding,
+                                 bias=bias)]
+            layers += [nn.BatchNorm2d(num_features=out_channels)]
+            layers += [nn.ReLU()]
+
+            cbr = nn.Sequential(*layers)
+
+            return cbr
+
+        # Contracting path
+        self.enc1_1 = CBR2d(in_channels=1, out_channels=64)
+        self.enc1_2 = CBR2d(in_channels=64, out_channels=64)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc2_1 = CBR2d(in_channels=64, out_channels=128)
+        self.enc2_2 = CBR2d(in_channels=128, out_channels=128)
+
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc3_1 = CBR2d(in_channels=128, out_channels=256)
+        self.enc3_2 = CBR2d(in_channels=256, out_channels=256)
+
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc4_1 = CBR2d(in_channels=256, out_channels=512)
+        self.enc4_2 = CBR2d(in_channels=512, out_channels=512)
+
+        self.pool4 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc5_1 = CBR2d(in_channels=512, out_channels=1024)
+
+        # Expansive path
+        self.dec5_1 = CBR2d(in_channels=1024, out_channels=512)
+
+        self.unpool4 = nn.ConvTranspose2d(in_channels=512, out_channels=512,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec4_2 = CBR2d(in_channels=2 * 512, out_channels=512)
+        self.dec4_1 = CBR2d(in_channels=512, out_channels=256)
+
+        self.unpool3 = nn.ConvTranspose2d(in_channels=256, out_channels=256,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec3_2 = CBR2d(in_channels=2 * 256, out_channels=256)
+        self.dec3_1 = CBR2d(in_channels=256, out_channels=128)
+
+        self.unpool2 = nn.ConvTranspose2d(in_channels=128, out_channels=128,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec2_2 = CBR2d(in_channels=2 * 128, out_channels=128)
+        self.dec2_1 = CBR2d(in_channels=128, out_channels=64)
+
+        self.unpool1 = nn.ConvTranspose2d(in_channels=64, out_channels=64,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec1_2 = CBR2d(in_channels=2 * 64, out_channels=64)
+        self.dec1_1 = CBR2d(in_channels=64, out_channels=64)
+
+        self.fc = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0, bias=True)
+        
+    def forward(self, x):
+        enc1_1 = self.enc1_1(x)
+        enc1_2 = self.enc1_2(enc1_1)
+        pool1 = self.pool1(enc1_2)
+
+        enc2_1 = self.enc2_1(pool1)
+        enc2_2 = self.enc2_2(enc2_1)
+        pool2 = self.pool2(enc2_2)
+
+        enc3_1 = self.enc3_1(pool2)
+        enc3_2 = self.enc3_2(enc3_1)
+        pool3 = self.pool3(enc3_2)
+
+        enc4_1 = self.enc4_1(pool3)
+        enc4_2 = self.enc4_2(enc4_1)
+        pool4 = self.pool4(enc4_2)
+
+        enc5_1 = self.enc5_1(pool4)
+
+        dec5_1 = self.dec5_1(enc5_1)
+
+        unpool4 = self.unpool4(dec5_1)
+        cat4 = torch.cat((unpool4, enc4_2), dim=1)
+        dec4_2 = self.dec4_2(cat4)
+        dec4_1 = self.dec4_1(dec4_2)
+
+        unpool3 = self.unpool3(dec4_1)
+        cat3 = torch.cat((unpool3, enc3_2), dim=1)
+        dec3_2 = self.dec3_2(cat3)
+        dec3_1 = self.dec3_1(dec3_2)
+
+        unpool2 = self.unpool2(dec3_1)
+        cat2 = torch.cat((unpool2, enc2_2), dim=1)
+        dec2_2 = self.dec2_2(cat2)
+        dec2_1 = self.dec2_1(dec2_2)
+
+        unpool1 = self.unpool1(dec2_1)
+        cat1 = torch.cat((unpool1, enc1_2), dim=1)
+        dec1_2 = self.dec1_2(cat1)
+        dec1_1 = self.dec1_1(dec1_2)
+
+        x = self.fc(dec1_1)
+
+        return x
+
+## 라이브러리 추가하기  
+import argparse
+
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+#from torch.utils.tensorboard import SummaryWriter
+
+from model import UNet
+from dataset_sarco import *
+from util import *
+
+import matplotlib.pyplot as plt
+
+from torchvision import transforms, datasets
+
+## Parser 생성하기
+parser = argparse.ArgumentParser(description="Train the UNet",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter) 
+
+parser.add_argument("--lr", default=1e-4, type=float, dest="lr")
+parser.add_argument("--batch_size", default=1, type=int, dest="batch_size")
+parser.add_argument("--num_epoch", default=500, type=int, dest="num_epoch")
+
+parser.add_argument("--data_dir", default=r"C:\Users\user\1_LIMTAEYOON\1_Unet\dataset", type=str, dest="data_dir")
+parser.add_argument("--ckpt_dir", default=r"C:\Users\user\1_LIMTAEYOON\1_Unet\checkpoint", type=str, dest="ckpt_dir")
+parser.add_argument("--log_dir", default= r"C:\Users\user\1_LIMTAEYOON\1_Unet\log", type=str, dest="log_dir")
+parser.add_argument("--result_dir", default=r"C:\Users\user\1_LIMTAEYOON\1_Unet\result", type=str, dest="result_dir")
+
+parser.add_argument("--mode", default="train", type=str, dest="mode")
+parser.add_argument("--train_continue", default="off", type=str, dest="train_continue")
+
+args = parser.parse_args(args=[])
+
+## 트레이닝 파라메터 설정하기
+lr = args.lr
+batch_size = args.batch_size
+num_epoch = args.num_epoch
+
+data_dir = args.data_dir
+ckpt_dir = args.ckpt_dir
+log_dir = args.log_dir
+result_dir = args.result_dir
+
+mode = args.mode
+train_continue = args.train_continue
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+print("learning rate: %.4e" % lr)
+print("batch size: %d" % batch_size)
+print("number of epoch: %d" % num_epoch)
+print("data dir: %s" % data_dir)
+print("ckpt dir: %s" % ckpt_dir)
+print("log dir: %s" % log_dir)
+print("result dir: %s" % result_dir)
+print("mode: %s" % mode)
+
+## 디렉토리 생성하기
+if not os.path.exists(result_dir):
+    os.makedirs(os.path.join(result_dir, 'png'))
+    os.makedirs(os.path.join(result_dir, 'numpy')) 
+
+
+## 네트워크 학습하기
+if mode == 'train':
+    transform = transforms.Compose([Normalization(mean=0.5, std=0.5), RandomFlip(), ToTensor()])
+
+    dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'), transform=transform)
+    print("len(dataset_train):", len(dataset_train))
+    loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=0)
+
+    dataset_val = Dataset(data_dir=os.path.join(data_dir, 'val'), transform=transform)
+    loader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=False, num_workers=0)                                                                            
+
+    # 그밖에 부수적인 variables 설정하기
+    num_data_train = len(dataset_train)
+    num_data_val = len(dataset_val)
+
+    num_batch_train = np.ceil(num_data_train / batch_size)
+    num_batch_val = np.ceil(num_data_val / batch_size)
+    
+    transform = transforms.Compose([Normalization(mean=0.5, std=0.5), ToTensor()])
+
+    dataset_test = Dataset(data_dir=os.path.join(data_dir, 'test'), transform=transform)
+    loader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False, num_workers=0)
+
+    # 그밖에 부수적인 variables 설정하기
+    num_data_test = len(dataset_test)
+
+    num_batch_test = np.ceil(num_data_test / batch_size)
+
+else:
+    transform = transforms.Compose([Normalization(mean=0.5, std=0.5), ToTensor()])
+
+    dataset_test = Dataset(data_dir=os.path.join(data_dir, 'test'), transform=transform)
+    loader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False, num_workers=0)
+
+    # 그밖에 부수적인 variables 설정하기
+    num_data_test = len(dataset_test)
+
+    num_batch_test = np.ceil(num_data_test / batch_size)
+
+## 네트워크 생성하기
+net = UNet().to(device)
+
+## 손실함수 정의하기
+fn_loss = nn.BCEWithLogitsLoss(pos_weight=torch.FloatTensor([1])).to(device)
+
+## Optimizer 설정하기
+optim = torch.optim.Adam(net.parameters(), lr=lr)
+
+## 그밖에 부수적인 functions 설정하기
+fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
+fn_denorm = lambda x, mean, std: (x * std) + mean
+fn_class = lambda x: 1.0 * (x > 0.5)
+
+# ## Tensorboard 를 사용하기 위한 SummaryWriter 설정
+# writer_train = SummaryWriter(log_dir=os.path.join(log_dir, 'train'))
+# writer_val = SummaryWriter(log_dir=os.path.join(log_dir, 'val'))
+
+## 네트워크 학습시키기
+st_epoch = 0
